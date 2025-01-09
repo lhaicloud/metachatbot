a9f08148cf0f761bcb50f95d10ce3b5e707292c5
lhaicloud <46161368+lhaicloud@users.noreply.github.com>
2025-01-09 13:40:02 +0800

    UPDATE 87

--- index.js
+++ index.js
@@ -415,12 +415,13 @@ function handleUserMessage(senderId, message) {
         //     break;
         case 'ask_account':
             // Validate the account number (replace with your actual verification logic)
-            if (validateAccountNumber(message,senderId)) {
-                userSessions[senderId].step = 'ask_otp_method';
-                sendOTPChoiceMenu(senderId);
-            } else {
-                sendMessage(senderId, 'Sorry, the account number you provided is invalid. See image for your reference.',true);
-            }
+            console.log(validateAccountNumber(message,senderId))
+            // if (validateAccountNumber(message,senderId)) {
+            //     userSessions[senderId].step = 'ask_otp_method';
+            //     sendOTPChoiceMenu(senderId);
+            // } else {
+            //     sendMessage(senderId, 'Sorry, the account number you provided is invalid. See image for your reference.',true);
+            // }
             break;
         case 'ask_otp_method':
             if (message === "MOBILE NUMBER" || message === "EMAIL ADDRESS") {

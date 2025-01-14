e212f2b3b086aa85fc1bca6261fd69bcf42d4bc7
lhaicloud <46161368+lhaicloud@users.noreply.github.com>
2025-01-14 08:39:22 +0800

    UPDATE 157

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get("/webhook", (req, res) => {
     const mode = req.query["hub.mode"];
     const token = req.query["hub.verify_token"];
     const challenge = req.query["hub.challenge"];
-
+    console.log(token)
     if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
         console.log("Webhook verified!");
         res.status(200).send(challenge);
